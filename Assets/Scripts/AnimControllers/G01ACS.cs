82344a063ff311800ba816367bfef293303def8f
Alejandro-1003 <95064947+Alejandro-1003@users.noreply.github.com>
2023-06-26 13:00:49 -0500

    Movimintos con tecla add

---
+++ Assets/Scripts/AnimControllers/G01ACS.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G01ACS : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    public int currentAnimation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationController();
+    }
+
+    private void animationController()
+    {
+        currentAnimation = 1;
+
+        //correr
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            spriteRenderer.flipX = true;
+            currentAnimation = 3;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            spriteRenderer.flipX = false;
+            currentAnimation = 3;
+        }
+       
+       //Deslisarse
+
+        if (Input.GetKey(KeyCode.A))
+            currentAnimation = 4;
+
+        //Saltar
+        if (Input.GetKey(KeyCode.UpArrow))
+            currentAnimation = 6;
+
+        //Atacar
+
+        if (Input.GetKey(KeyCode.X))
+            currentAnimation = 5;
+
+        //Morir
+          if (Input.GetKey(KeyCode.D))
+            currentAnimation = 7;
+    
+
+
+        animator.SetInteger("Estado", currentAnimation);
+
+    }
+}
---
+++ Assets/Scripts/G01MovimientoControl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G01MovimientoControl : MonoBehaviour
+{
+     private Rigidbody2D rb;
+    private float yVelocity = 0f;
+    public float xVelocity = 20f;
+    public float jumpForce = 100f;
+    public float gravityScale = 1f;
+    public float mass = 1f;
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravityScale; // Configurar la escala de gravedad del Rigidbody2D
+        rb.mass = mass; // Configurar la masa del Rigidbody2D
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yVelocity = rb.velocity.y;
+        rb.velocity = new Vector2(0, yVelocity);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+            rb.velocity = new Vector2(-xVelocity, yVelocity);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.velocity = new Vector2(xVelocity, yVelocity);
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) && !isJumping)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            isJumping = true;
+        }
+    }
+
+    // Detectar colisiones con el suelo para restablecer el indicador de salto
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
