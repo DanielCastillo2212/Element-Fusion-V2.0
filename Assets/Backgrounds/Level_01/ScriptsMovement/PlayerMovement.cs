e46f58622e31bfcc870d9b407527bf025763ed7d
DanielCastillo2212 <danielandre.castillo@gmail.com>
2023-06-26 14:40:44 -0500

    Follow Camera

--- Assets/Backgrounds/Level_01/ScriptsMovement/PlayerMovement.cs
+++ Assets/Backgrounds/Level_01/ScriptsMovement/PlayerMovement.cs
@@ -51,6 +51,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            Debug.Log("Suelo detectada");
             isJumping = false;
         }
     }
